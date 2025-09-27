762face5cf68179a4d60ced0daa9322aa18f4f28
yrlmanoharreddy <yrlmanoharreddymeda@gmail.com>
2025-09-26 21:29:38 -0500

    Added Exception handler for customer already exception

--- accounts/src/main/java/com/manuBank/accounts/exception/GlobalExceptionHandler.java
+++ accounts/src/main/java/com/manuBank/accounts/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.manuBank.accounts.dto.ErrorResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
@@ -16,6 +17,7 @@ public class GlobalExceptionHandler {
 
     }
 
+    @ExceptionHandler(CustomerAlreadyExistsException.class)
     public ResponseEntity<ErrorResponseDto> handleCustomerAlreadyExistsException(CustomerAlreadyExistsException exception, WebRequest webrequest)
     {
         ErrorResponseDto errorResponseDto = new ErrorResponseDto(
